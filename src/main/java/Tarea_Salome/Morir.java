8a4556848d88c30ba354944e897a3756d519fdc4
salome30261412 <trujillo.salome.5328@eam.edu.co>
2025-03-10 23:44:55 -0500

    se añadio clase morir

---
+++ src/main/java/Tarea_Salome/Morir.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tarea_Salome;
+
+import static Tarea_Salome.Libro.CATEGORIA;
+
+/**
+ *
+ * @author Salome
+ */
+public class Morir {
+    System.out.println("vamos a morir"); 
+}
